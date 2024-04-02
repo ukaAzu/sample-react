efa419ce435a330b09ee665f0c47a045845130f4
ukaAzu <yukan1988@gmail.com>
2024-04-02 18:01:47 +0900

    p.70

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  const msg = "これはサンプルのメッセージです"
   return (
     <div className="App">
       <h1>React sample</h1>
       <p>This is sample application.</p>
+      <div>{msg}</div>
     </div>
   );
 }
