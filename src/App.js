933aec9968fa18c1916ae13d48dba4d9d51d0cbb
ukaAzu <yukan1988@gmail.com>
2024-04-03 13:49:12 +0900

    p.76

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App(props) {
+  var counter = 0
+  const doClick = () => {
+    counter++
+  }
   return (
     <div className="App">
       <h1 className={props.color}>{props.title}</h1>
-      <p className={props.color}>{props.message}</p>
+      <p className={props.color, "clickable"} onClick={doClick}>counter: {counter}.</p>
     </div>
   );
 }
