db12d43012f116953b3abac0047158bc9f65b84d
ukaAzu <yukan1988@gmail.com>
2024-04-03 14:45:18 +0900

    p.92

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App(props) {
-  const data = [
-    { name: "Taro", mail: "Taro@example.com" },
-    { name: "Hanako", mail: "Hanako@example.com" },
-    { name: "Sachiko", mail: "Sachiko@example.com" }
-  ]
+  var [flag, setFlag] = useState(true)
+
+  const doClick = (event) => {
+    setFlag(!flag)
+  }
 
   return (
     <div className="App">
       <h1>{props.title}</h1>
-      <ul>
-        {data.map((item, key) => {
-          return (<li>{item.name} [{item.mail}]</li>)
-        })}
-      </ul>
+      <p className={flag ? "classA" : "classB"}>{flag ? "ON" : "OFF"}です</p>
+      <button className='button' onClick={doClick}>Click</button>
     </div>
   );
 }
