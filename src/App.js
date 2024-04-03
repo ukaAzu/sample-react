1ba345b9a80716e380823aff4c7534d73354fb3b
ukaAzu <yukan1988@gmail.com>
2024-04-03 14:20:59 +0900

    p.85

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App(props) {
-  const [input, setInput] = useState("")
-  const [message, setMessage] = useState("お名前は？")
+  const [input, setInput] = useState(1)
+  const [message, setMessage] = useState("数値を入力")
   const doInput = (event) => {
     setInput(event.target.value)
   }
-  const doClick = () => {
-    setMessage("こんにちは、" + input + "さん！")
-  }
+
+  useEffect(() => {
+    var prime = true
+    if (input == 1) {
+      prime = false
+    } else {
+      for (var i = 2; i <= input / 2; i++) {
+        if (input % i === 0) {
+          prime = false
+          break
+        }
+      }
+    }
+    setMessage(prime ? "素数" : "素数ではない")
+  }, [input])
+
   return (
     <div className="App">
       <h1 className={props.color}>{props.title}</h1>
       <p className={'${props.color} clickable'}>{message}</p>
       <div>
-        <input type="text" onChange={doInput} />
-        <button onClick={doClick}>Click</button>
+        <input type="number" min="1" onChange={doInput} />
       </div>
     </div>
   );
