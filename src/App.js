b25f73019023da887f565902393be90b96082c7d
ukaAzu <yukan1988@gmail.com>
2024-04-03 15:01:38 +0900

    p.95

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App(props) {
-  var [flag, setFlag] = useState(true)
+  var [count, setCount] = useState(0)
+  var [data, setData] = useState([
+    {
+      position: "absolute",
+      left: "0px",
+      top: "0px",
+      width: "100%",
+      height: "100%",
+      backgroundColor: "#fff0",
+    }
+  ])
 
   const doClick = (event) => {
-    setFlag(!flag)
+    const ob = {
+      position: "absolute",
+      left: (event.pageX - 50) + "px",
+      top: (event.pageY - 50) + "px",
+      width: "100px",
+      height: "100px",
+      backgroundColor: "#ff000066",
+      borderRadius: "50%"
+    }
+    data.push(ob)
+    setCount(count + 1)
   }
 
   return (
     <div className="App">
       <h1>{props.title}</h1>
-      <p className={flag ? "classA" : "classB"}>{flag ? "ON" : "OFF"}です</p>
-      <button className='button' onClick={doClick}>Click</button>
+      <p>{count} objects</p>
+      <div onClick={doClick}>
+        {data.map((item, key) => {
+          return (<div style={item} key={key}></div>)
+        })}
+      </div>
     </div>
   );
 }
